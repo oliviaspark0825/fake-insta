4f62fe36b6ae282af1d1affc1abaa0263a593297
oliviaspark0825 <suhyunpark0825@gmail.com>
2019-04-11 05:35:04 +0000

    01_make post Model & admin setting

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+# setting 끝나면 models하기
 # Create your models here.
+class Post(models.Model):
+    content = models.TextField()
+    
+    
+    def __str__(self):
+        return self.content
