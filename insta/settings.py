550f98c1e9e129e32ce14d2ea3c36c733d0f9a97
oliviaspark0825 <suhyunpark0825@gmail.com>
2019-04-12 05:04:50 +0000

    03_make favicon

--- insta/settings.py
+++ insta/settings.py
@@ -121,6 +121,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'insta', 'static'),
+]
 # 이미지를 넣기 위해서는 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
